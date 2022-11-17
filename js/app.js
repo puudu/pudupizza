d1cb50bcb52903c8f063b54f04b0b012a5c85466
ducktracio <luispdonoso.a@hotmail.com>
2022-11-17 02:04:38 -0300

    js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const pizzas = [
+    { id: 1, nombre: "Napolitana", tamanio: "Familiar", precio: 7990 },
+    { id: 1, nombre: "Pepperoni", tamanio: "Familiar", precio: 7990 },
+    { id: 1, nombre: "Italiana", tamanio: "Familiar", precio: 7990 },
+    { id: 1, nombre: "Napolitana", tamanio: "Familiar", precio: 7990 },
+];
+
+let carrito = []
